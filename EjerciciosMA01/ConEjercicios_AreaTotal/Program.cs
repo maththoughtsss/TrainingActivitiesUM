68113d1b2601a3ca9717f154837507e68ab73f1d
maththoughtsss <jonathan.munoz01@mayor.cl>
2023-04-08 18:46:11 -0400

    Inicio EjerciciosMA01 - wpfEjercicios_Volumen y ConEjercicios_AreaTotal resueltos-

---
+++ EjerciciosMA01/ConEjercicios_AreaTotal/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConEjercicios_AreaTotal
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Declaración de Variables
+            string? strHeight,strRadius;
+            double height, radius, cylinderArea, pi, radiusSquared;
+            pi = 3.14;
+            
+            //Título
+            Console.WriteLine("Cálculo de Área de un Cilindro Recto \n");
+            //Input
+            Console.WriteLine("Ingrese la altura del cilindro: ");            
+            strHeight = Console.ReadLine();
+            Console.WriteLine("Ingrese el radio de cilindro: ");            
+            strRadius = Console.ReadLine();
+            //Función de Cálculo
+            double.TryParse(strHeight, out height);
+            double.TryParse(strRadius, out radius);
+
+            radiusSquared = radius * radius;
+            
+            cylinderArea= 2 * pi * radius * height + 2 * pi * radiusSquared;
+            //Output
+            Console.WriteLine("+++++++ +++++++ +++++++ +++++++ +++++++ +++++++");
+            Console.WriteLine($"\t Área total del cilindro: {cylinderArea} u2");
+            Console.WriteLine("+++++++ +++++++ +++++++ +++++++ +++++++ +++++++");
+            //Salida
+            Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjerciciosMA01/wfpEjercicios_Volumen/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wfpEjercicios_Volumen
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Por convención las variables y funciones del programa están declaradas en inglés, sin embargo, la interfaz con el usuario retorna los mensajes y resultados en idioma español. JAMM
+        //Declaración de Variables Globales
+
+        double pi = 3.14;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            //Limpieza de Inputs y Output
+            this.txtHeight.Text = string.Empty;
+            this.txtRadius.Text = string.Empty;
+            this.lblVolumeResult.Content = string.Empty;
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            //Declaración de Varaiables
+            String strHeight, strRadius;
+            double height, radius, volume, radiusSquared;
+
+            strHeight = this.txtHeight.Text;
+            strRadius = this.txtRadius.Text;
+
+            double.TryParse(strHeight, out  height);
+            double.TryParse(strRadius, out radius);
+
+            //Cálculo de Volumen
+            radiusSquared = radius * radius;
+            volume = pi * height * radiusSquared;
+
+            //Visualización en Ventana
+            this.lblVolumeResult.Content = volume.ToString();
+        }
+    }
+}
