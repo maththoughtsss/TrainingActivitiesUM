f870293659d1b76f4ba8898ccb0633b4021f3eb8
maththoughtsss <jonathan.munoz01@mayor.cl>
2023-04-07 20:10:22 -0400

    Inicio de Actividades Formativas UM - MA01-015 - MA01-017

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,138 @@
+﻿namespace AreaCalculator
+{
+    class Program
+    {
+        //Por convención las variables y funciones del programa están indicadas en inglés, sin embargo, la interfaz con es usuario retorna los mensajes en idioma español.
+        public static void Main(string[] args)
+        {
+            //Bienvenida y Menú de Selección
+            Selection();
+        }
+        public static void Selection()
+        {
+            //Declaración de Variables de Selección
+            string? strShape;
+            int numShape;
+
+            //Enunciado de Selección
+            Console.WriteLine("Bienvenido a la aplicación para Cálculo de Áreas \n");
+            Console.WriteLine("Por favor, ingrese el tipo de figura a calcular: \n");
+            Console.WriteLine(" 1.- Circunferencia \n 2.- Rectángulo \n 3.- Triángulo");
+
+            //Conversión de String a Int para Selección
+            strShape = Console.ReadLine();
+            int.TryParse(strShape, out numShape);
+
+            //Switch de Selección (Elección de Función a utilizar)
+            switch(numShape)
+            {   
+                //case 1 - Corre Función para calcular el área de una circunferencia
+                case 1:
+                {
+                    circumferenceArea();
+                    break;
+                }
+                //case 2 - Corre Función para calcular el área de un rectángulo
+                case 2:
+                {
+                    rectangleArea();
+                    break;
+                }
+                //case 3 - Corre Función para calcular el área de un triángulo
+                case 3:
+                {
+                    triangleArea();
+                    break;
+                }
+            }
+        }
+        //Función de Retorno o Salida
+        public static void returnOrClose()
+        {
+            //Consulta para continuar o salir del programa
+            Console.WriteLine("Desea volver a calcular nuevamente (Y/N)");
+            string? numChoice = Console.ReadLine();            
+            if(numChoice == "Y") //Retorna a la función para un nuevo cálculo
+            {
+                circumferenceArea();
+            }
+            if(numChoice == "N") //Salida del programa
+            {
+                Console.WriteLine("Presione cualquier tecla para salir...");
+                Console.ReadKey();
+            }
+            else //Retorna de manera recursiva la función en caso de presionar cualquier otra tecla...
+            {
+                returnOrClose();
+            }            
+        }
+        public static void circumferenceArea()
+        //Función para calcular el área de una circunferencia
+        {
+            //Declaración de Variables
+            string? strRadius;
+            double NumRadius, circArea, pi;
+            pi = 3.14;
+            //Título
+            Console.WriteLine("CALCULAR EL ÁREA DE UNA CIRCUNFERENCIA \n");
+            //Input
+            Console.WriteLine("Ingresa el radio de la circunferencia: ");            
+            strRadius = Console.ReadLine();
+            //Función de Cálculo
+            double.TryParse(strRadius, out NumRadius);
+            circArea= NumRadius * NumRadius * pi;
+            //Output
+            Console.WriteLine($"Total Area: {circArea} u2");
+            //Retornar o Salir
+            returnOrClose();
+        }
+        public static void rectangleArea()
+        //Función para calcular el área de un rectángulo
+        {
+            //Delcaración de Variables
+            string? strHeight, strWidth;
+            double height, width, rectArea;
+            //Título
+            Console.WriteLine("CALCULAR EL ÁREA DE UN RECTÁNGULO \n");
+            //Inputs
+            //Altura
+            Console.WriteLine("Ingresa la altura del rectángulo: ");            
+            strHeight = Console.ReadLine();
+            //Ancho
+            Console.WriteLine("Ingresa el ancho del rectángulo: ");            
+            strWidth = Console.ReadLine();
+            //Conversión de Variables y Función de Cálculo
+            double.TryParse(strHeight, out height);
+            double.TryParse(strWidth, out width);
+            rectArea = height * width;
+            //Output
+            Console.WriteLine($"Área total del Rectángulo: {rectArea} u2");
+            //Retornar o Salir
+            returnOrClose();
+        }
+        public static void triangleArea()
+        //Función para calcular el área de un triángulo
+        {
+            //Declaración de Variables
+            string? strHeight, strWidth;
+            double height, width, triArea;
+            //Título
+            Console.WriteLine("CALCULAR EL ÁREA DE UN TRIÁNGULO \n");
+            //Inputs
+            //Altura
+            Console.WriteLine("Ingresa la altura del triángulo: ");            
+            strHeight = Console.ReadLine();
+            //Base
+            Console.WriteLine("Ingresa el ancho del triángulo: ");            
+            strWidth = Console.ReadLine();
+            //Conversión de Variable y Función de Cálculo
+            double.TryParse(strHeight, out height);
+            double.TryParse(strWidth, out width);
+            triArea = height * width / 2;
+            //Output
+            Console.WriteLine($"Área total del Triángulo: {triArea} u2");
+            //Retornar o Salir
+            returnOrClose();
+        }
+    }
+}
---
+++ wpfIVACalculator/wpfIVACalculator/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfIVACalculator
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Declaración de Variables Globales
+        double price;
+        double iva = 0.19;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            //Declaración de Variables Locales
+            string strPrice;
+            double ivaPrice,priceWithoutIVA;
+            //Captura de variable como tipo String
+            strPrice = txtPrice.Text;
+            //Conversión a tipo double
+            double.TryParse(strPrice, out price);
+            //Cálculo de IVA
+            ivaPrice = price * iva;
+            //Precio sin IVA
+            priceWithoutIVA = price - ivaPrice;
+            //Mostrar en MessageBox
+            MessageBox.Show("El IVA correspondiente al precio es: " + ivaPrice);
+            //Mostrar el IVA en Ventana
+            this.lblAmount.Content = ivaPrice.ToString();
+            this.lblOriginalPriceAmount.Content = priceWithoutIVA.ToString();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtPrice.Text = string.Empty;
+            this.lblAmount.Content = string.Empty;
+            this.lblOriginalPriceAmount.Content = string.Empty;
+        }
+    }
+}
